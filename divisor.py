d14ceed23f5a37471941803e565e447e816acc91
yix90 <kyixuan@gmail.com>
2017-05-21 10:00:41 +0800

    Create divisor.py

---
+++ divisor.py
@@ -0,0 +1,16 @@
+while True:
+    try:
+        num = int(input("Enter a number: "))
+    except:
+        continue
+    if num > 1000000:
+        print ("The number keyed is too large.")
+        continue
+    break
+a = 1
+mul = []
+while a <= num:
+    if num%a == 0:
+        mul.append(a)
+    a = a + 1
+print ("The multiples of", num, "is", mul)
