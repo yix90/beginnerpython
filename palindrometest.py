2c0519eb4ed8b09c3b139eac72800bdb12e86f4d
yix90 <kyixuan@gmail.com>
2017-05-21 11:39:51 +0800

    Create palindrometest.py
    Shortest form of testing if a string is a palindrome

---
+++ palindrometest.py
@@ -0,0 +1,7 @@
+a = input ("Palindrome test: Enter a word--")
+n = 0
+if a[:] == a[::-1]:
+  nothing = " "
+else: nothing = " not "
+print ("The word you entered is" + nothing + "a palindrome.")
+  
